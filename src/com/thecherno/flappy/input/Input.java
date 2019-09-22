7b1d830548347f1cd24e6b2723618e75fc98fc1f
Chris Gruber <chris@chrisgruber.com>
2019-09-21 23:57:06 -0500

    GLFW_KEY_LAST is the highest value of any named key

--- src/com/thecherno/flappy/input/Input.java
+++ src/com/thecherno/flappy/input/Input.java
@@ -3,9 +3,11 @@ package com.thecherno.flappy.input;
 import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWKeyCallback;
 
+import static org.lwjgl.glfw.GLFW.*;
+
 public class Input extends GLFWKeyCallback {
 	
-	public static boolean[] keys = new boolean[65536];
+	public static boolean[] keys = new boolean[GLFW_KEY_LAST];
 
 	public void invoke(long window, int key, int scancode, int action, int mods) {
 		keys[key] = action != GLFW.GLFW_RELEASE; 
